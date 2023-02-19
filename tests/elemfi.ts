32f216d067916e04af3593ab74e41aaa65844796
0xWoo <keisukew53@gmail.com>
2023-02-19 12:14:04 -0500

    update SDK integration in test

--- tests/elemfi.ts
+++ tests/elemfi.ts
@@ -1,7 +1,7 @@
 import fs from "fs";
 import { setProvider, AnchorProvider } from "@coral-xyz/anchor";
 import { Keypair } from "@solana/web3.js";
-import { Realm } from "@elemfi/sdk";
+import { ConnectedWallet, ElemFiSDK, Realm } from "@elemfi/sdk";
 
 const WALLET_KEYPAIR = Keypair.fromSecretKey(
   new Uint8Array(JSON.parse(fs.readFileSync(process.env.ANCHOR_WALLET as string, { encoding: "utf8" })))
@@ -9,10 +9,12 @@ const WALLET_KEYPAIR = Keypair.fromSecretKey(
 
 describe("elemfi", () => {
   const provider = AnchorProvider.env();
+  const wallet = new ConnectedWallet(provider.connection, provider.publicKey);
+  const sdk = new ElemFiSDK(provider);
   setProvider(provider);
 
   it("should create a realm", async () => {
-    const { tx, realm } = await Realm.create(provider, {
+    const { tx, realm } = await Realm.create(sdk.program, wallet, {
       realmKP: Keypair.generate(),
       delegator: Keypair.generate().publicKey,
       approver: Keypair.generate().publicKey,
@@ -21,6 +23,6 @@ describe("elemfi", () => {
 
     tx.sign([WALLET_KEYPAIR]);
     const signature = await provider.connection.sendTransaction(tx);
-    await realm.wallet.confirmTransaction(signature);
+    await wallet.confirmTransaction(signature);
   });
 });
